a277c08f8a5ac9e5c6bae715f5d0fdf0c6da2eb2
dansusman <frenchminister@gmail.com>
2022-10-16 07:48:35 -0400

    Add "Explore" title back

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -4,17 +4,18 @@ import ExploreComponent from "./ExploreComponent.js";
 
 function exploreComponent() {
     $("#wd-explore").append(`
-  <div class="row mt-4">
-   <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-    ${NavigationSidebar()}
+    <h2 class="pt-4">Explore</h2>
+   <div class="row mt-4">
+      <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+      ${NavigationSidebar()}
+      </div>
+      <div class="col-10 col-md-10 col-lg-7 col-xl-6 text-white">
+         ${ExploreComponent()}
+      </div>
+      <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
+      ${WhoToFollowList()}
+      </div>
    </div>
-   <div class="col-10 col-md-10 col-lg-7 col-xl-6 text-white">
-      ${ExploreComponent()}
-   </div>
-   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
-    ${WhoToFollowList()}
-   </div>
-  </div>
    `);
 }
 $(exploreComponent);
