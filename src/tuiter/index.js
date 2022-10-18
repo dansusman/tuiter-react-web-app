a8908c0666bcffe202ae45d1c9ceb9ea5aebc1fb
dansusman <frenchminister@gmail.com>
2022-10-18 12:59:18 -0400

    MVP for Tuiter Portion of A6

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-import Nav from "../nav";
+import ExploreComponent from "./explore";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
     return (
-        <div>
-            <Nav />
-            <h1>Tuiter</h1>
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore" />
+            </div>
+            <div
+                className="col-10 col-md-10 col-lg-7 col-xl-6"
+                style={{ position: "relative" }}
+            >
+                <ExploreComponent />
+            </div>
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                <WhoToFollowList />
+            </div>
         </div>
     );
 }
+
 export default Tuiter;
