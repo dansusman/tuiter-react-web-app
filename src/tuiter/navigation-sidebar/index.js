aadf73cd98de9e69326f1d94a5d2a8a31ece2016
dansusman <frenchminister@gmail.com>
2022-11-01 13:34:18 -0400

    Hopefully fix Netlify confusing domains

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -6,7 +6,10 @@ import tabsArray from "./tabs.json";
 const NavigationSidebar = () => {
     const { pathname } = useLocation();
     const paths = pathname ? pathname.split("/") : [];
-    const active = paths[paths.length - 1] || "home";
+    var active = paths[paths.length - 1] || "";
+    if (active === "" || active === "tuiter") {
+        active = "home";
+    }
     return (
         <div className="list-group pt-1">
             {tabsArray.map((tab) => (
