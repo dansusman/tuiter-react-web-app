686da5470f30af1dd4c8e9b41394f561fad5aca8
dansusman <frenchminister@gmail.com>
2022-11-01 12:04:13 -0400

    Port WhoToFollow and PostSummary to Redux

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,24 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
+import { Provider } from "react-redux";
 import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
+import tuitsReducer from "./tuits/tuits-reducer";
+import whoReducer from "./reducers/who-reducer";
 import WhoToFollowList from "./who-to-follow-list";
+
+const store = configureStore({
+    reducer: { who: whoReducer, tuits: tuitsReducer },
+});
 
 function Tuiter() {
     return (
-        <div className="row mt-4">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar tab="explore" />
+        <Provider store={store}>
+            <div className="row mt-4">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar tab="explore" />
+                </div>
+                <div
+                    className="col-10 col-md-10 col-lg-7 col-xl-6"
+                    style={{ position: "relative" }}
+                >
+                    <ExploreComponent />
+                </div>
+                <div className="d-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList />
+                </div>
             </div>
-            <div
-                className="col-10 col-md-10 col-lg-7 col-xl-6"
-                style={{ position: "relative" }}
-            >
-                <ExploreComponent />
-            </div>
-            <div className="d-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList />
-            </div>
-        </div>
+        </Provider>
     );
 }
 
--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import postsArray from "./posts.json";
 import PostSummaryItem from "./post-summary-item";
+import { useSelector } from "react-redux";
 
 const PostSummaryList = () => {
+    const postsArray = useSelector((state) => state.tuits);
     return (
         <ul className="list-group">
             {postsArray.map((post) => (
