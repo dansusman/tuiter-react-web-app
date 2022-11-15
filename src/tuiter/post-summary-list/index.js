5c815ede51cb4de919c3df992c8753c488f0ab68
dansusman <frenchminister@gmail.com>
2022-11-15 09:24:52 -0500

    Fix netlify build issues

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,18 +1,19 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+import React from "react";
 import PostSummaryItem from "./post-summary-item";
-import { useDispatch, useSelector } from "react-redux";
-import { findTuitsThunk } from "../../services/tuits-thunks";
+// import { useDispatch, useSelector } from "react-redux";
+// import { findTuitsThunk } from "../../services/tuits-thunks";
 import posts from "./posts.json";
 
 const PostSummaryList = () => {
-    const { tuits, loading } = useSelector((state) => state.tuitsData);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(findTuitsThunk());
-    }, []);
+    // const { tuits, loading } = useSelector((state) => state.tuitsData);
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     dispatch(findTuitsThunk());
+    // }, []);
     return (
         <ul className="list-group">
-            {loading && <li className="list-group-item">Loading...</li>}
+            {/* {loading && <li className="list-group-item">Loading...</li>} */}
             {posts.map((post) => (
                 <PostSummaryItem key={post._id} post={post} />
             ))}
